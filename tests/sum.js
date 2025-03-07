eb470f60010af5561587ce7113babdda8287dfef
SamuelLucasVieira <samuellucasvieira.melo@gmail.com>
2025-03-07 20:02:05 -0300

    Forçando reexecução do workflow

---
+++ tests/sum.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+    console.log("Hello World"); // Mensagem no console
+    return a + b; // Retorna o valor correto para os testes
+  }
+  
+  module.exports = sum;
+  
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,8 +1,5 @@
-function sum(a, b) {
-    return a + b;
-  }
-  
-  test('soma 1 + 2 e retorna 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-  
+const sum = require('./sum'); // Importa a função sum.js
+
+test('Deve somar 1 + 2 e retornar 3', () => {
+  expect(sum(1, 2)).toBe(3); // Verifica se retorna o valor correto
+});
